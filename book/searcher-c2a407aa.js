6e83557173e5bc129f4f624e89170995e74c4104
Zhang Yunlu <zhang.yunlu@qq.com>
2026-04-03 22:09:32 +0800

    更新联合

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-70c6b5de.js',
+                path_to_root + 'searchindex-b54cf221.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
