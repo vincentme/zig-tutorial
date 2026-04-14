5a4c00d5b5893bc309a865ead5af8e7e73348a04
Zhang Yunlu <zhang.yunlu@qq.com>
2026-04-14 22:13:42 +0800

    修改前八章

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8e831d08.js',
+                path_to_root + 'searchindex-d0c1d12c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
